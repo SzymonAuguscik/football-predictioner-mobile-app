f4f569e5910de7dbda380ddde9c0fa9e2d58c80a
Szymon Auguścik <szyauguscik@gmail.com>
2021-04-18 14:16:42 +0200

    Implement basic layout for predict activity

--- app/src/main/java/com/example/footballpredictioner/PredictActivity.kt
+++ app/src/main/java/com/example/footballpredictioner/PredictActivity.kt
@@ -2,11 +2,32 @@ package com.example.footballpredictioner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.footballpredictioner.adapters.PredictionAdapter
+import com.example.footballpredictioner.models.SinglePredictionRowModel
+import com.squareup.picasso.Picasso
 
 class PredictActivity : AppCompatActivity() {
+    private lateinit var predictionLogo: ImageView
+    private lateinit var predictionButton: Button
+    private lateinit var predictionRecyclerView: RecyclerView
+    private lateinit var adapter: PredictionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_predict)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        predictionLogo = findViewById(R.id.predictLogo)
+        predictionButton = findViewById(R.id.predictionDetailsButton)
+        predictionRecyclerView = findViewById(R.id.predictionRecyclerView)
+
+        adapter = PredictionAdapter(arrayOf(), this)
+        predictionRecyclerView.adapter = adapter
+
+        val logoUrl = intent.getStringExtra("logoUrl")
+        Picasso.get().load(logoUrl).into(predictionLogo)
     }
 }
